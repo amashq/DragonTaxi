c13878f6c2fd641cf101ed5350608a92148c7f0b
amashq <A.Mashenkova@gmail.com>
2020-01-10 17:14:58 +0300

    Add method

--- src/main/java/com/example/Dragonss/repos/OrderRepo.java
+++ src/main/java/com/example/Dragonss/repos/OrderRepo.java
@@ -1,5 +1,6 @@
 package com.example.Dragonss.repos;
 
+import com.example.Dragonss.domain.Customer;
 import com.example.Dragonss.domain.Orrder;
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,4 +9,7 @@ import java.util.List;
 public interface OrderRepo extends CrudRepository<Orrder, Integer> {
 //    List<Orrder> findByCustomerId(Integer customerId);
     Orrder findOrrderById(Integer id);
+
+    Orrder findOrrderByCustomerAndStartAddressAndDestAddressAndClassDAndTimeTravel(
+            Customer customer, String startAddress, String destAddress, String classD, String timeTravel);
 }
